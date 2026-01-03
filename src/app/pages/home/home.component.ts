89fa0432ca56c00060d51995d1ad4cce7d9f2695
M4theusRibeiro <matheusribeiro3105@gmail.com>
2026-01-02 21:42:55 -0300

    Commit inicial

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import Swal from 'sweetalert2';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+
+  openWhatsApp() {
+    Swal.fire({
+      title: 'Fale conosco',
+      text: 'Você será redirecionado para o WhatsApp',
+      icon: 'info',
+      confirmButtonText: 'Continuar',
+      confirmButtonColor: '#7b4dff'
+    }).then(() => {
+      window.open('https://wa.me/5500000000000', '_blank');
+    });
+  }
+}
