f68f60cd831f5728bf4f88d108e1aea9e8254693
M4theusRibeiro <matheusribeiro3105@gmail.com>
2026-01-02 23:36:17 -0300

    add: navbar

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -3,13 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  menuOpen = false;
 
-  constructor() { }
-
-  ngOnInit() {
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
   }
+  constructor() {}
 
+  ngOnInit() {}
 }
